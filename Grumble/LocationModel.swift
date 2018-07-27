e3b2a20f66e0ef1579c109cd61cee07c1ac1b0ed
Luis <luis@kylees-macbook-pro.local>
2018-07-27 16:11:11 -0400

    first version of locationModel

---
+++ Grumble/LocationModel.swift
@@ -0,0 +1,43 @@
+//
+//  LocationModel.swift
+//  Grumble
+//
+//  Created by Luis on 7/27/18.
+//  Copyright © 2018 SkyCloud. All rights reserved.
+// Object model that holds information parsed from MySQL database
+//container to store incoming data
+
+import Foundation
+
+class LocationModel: NSObject {
+    //properties
+    
+    var foodName: String?
+    var foodImage: String?
+    var latitude: String?
+    var longitude: String?
+    
+    
+    //emtpy constructor
+    override init()
+    {
+    
+    }
+    
+    //construct with properties of object (foodname, foodimage, longitude, latidude)
+    
+    init(name: String, image: String, latitude: String, longitude: String)
+    {
+        self.foodName = name
+        self.foodImage = image
+        self.latitude =  latitude
+        self.longitude = longitude
+    
+    }
+    //prints objects current state
+    
+    override var description: String {
+        return "Name: \(foodName), image: \(foodImage), latitude: \(latitude), longitude: \(longitude)"
+    }
+    
+}
